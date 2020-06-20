c461b66ed83d202f96b4f0ef11d31c9031580ab1
myelysieiev <m.yelysieiev@astoundcommerce.com>
2020-06-21 00:31:58 +0300

    second section

--- dist/js/main.js
+++ dist/js/main.js
@@ -43,12 +43,59 @@ document.addEventListener('DOMContentLoaded', function () {
             el: '.swiper-scrollbar',
             draggable: false,
             dragClass: 'scrollbar-elem'
-          },
+        },
 
         // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev'
+        },
+
+        breakpoints: {
+
+            320: {
+                direction: 'vertical',
+                slidesPerView: 8,
+                slidesPerGroup: 8,
+                shortSwipes: false,
+                longSwipes: false,
+                watchOverflow: true,
+                allowTouchMove: false,
+                spaceBetween: 60
+            },
+            480: {
+                direction: 'vertical',
+                slidesPerView: 8,
+                slidesPerGroup: 8,
+                shortSwipes: false,
+                longSwipes: false,
+                watchOverflow: true,
+                allowTouchMove: false,
+                spaceBetween: 60
+            },
+
+            578: {
+                direction: 'vertical',
+                slidesPerView: 8,
+                slidesPerGroup: 8,
+                shortSwipes: false,
+                longSwipes: false,
+                watchOverflow: true,
+                allowTouchMove: false,
+                spaceBetween: 60
+            },
+
+
+            769: {
+                direction: 'horizontal',
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+                watchSlidesVisibility: true,
+                spaceBetween: 20,
+                allowTouchMove: true
+            },
+
+
         }
     })
 });
@@ -63,5 +110,31 @@ document.addEventListener('click', function (event) {
         target.classList.add('active');
         initializeSlickPostition();
         slideProductPosition(target.dataset.category);
+    } else if (target.classList.contains('more-info')) {
+        target.style.display = 'none';
+        let btn = target.nextElementSibling;
+        let hover = target.previousElementSibling;
+        btn.style.display = 'block';
+        hover.style.opacity = '1';
+    } else if (target.classList.contains('cross')) {
+        let hover = target.parentElement;
+        let btn = hover.nextElementSibling;
+        let btnDisable = btn.nextElementSibling;
+        btnDisable.style.display = 'none';
+        btn.style.display = 'block';
+        hover.style.opacity = '0';
+    }
+});
+
+document.addEventListener('scroll', function(){
+    if(document.getElementById('scroll-breakpoint-price').getBoundingClientRect().y <= 100) {
+        document.querySelectorAll('.background-plant-price').forEach(function(el){
+            el.style.animationPlayState = 'running';
+        })
+    } else if(document.getElementById('scroll-breakpoint-step').getBoundingClientRect().y <= 50) {
+        document.querySelectorAll('.background-plant-step').forEach(function(el){
+            el.style.animationPlayState = 'running';
+        })
     }
 })
+
